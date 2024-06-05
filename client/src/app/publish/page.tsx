df69d980f5a3bab2f0d11497bf5fa14f632349aa
jp-quintana <juanquintana1996@gmail.com>
2024-06-05 17:29:42 -0300

    wip2: publish form

--- client/src/app/publish/page.tsx
+++ client/src/app/publish/page.tsx
@@ -1,11 +1,11 @@
 import MaxWidthContainer from '@/components/max-width-container';
-import PublishCard from '@/components/publish/publish-card';
+import PublishForm from '@/components/publish/publish-form';
 
 const PublishPage = () => {
   return (
     <MaxWidthContainer className="py-20 min-h-screen flex flex-col space-y-4">
       <h1 className="font-bold">Publicá tu producto</h1>
-      <PublishCard />
+      <PublishForm />
     </MaxWidthContainer>
   );
 };
--- client/src/components/publish/publish-form.tsx
+++ client/src/components/publish/publish-form.tsx
@@ -17,15 +17,16 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Image as ImageIcon } from 'lucide-react';
+import Image from 'next/image';
 
 const formSchema = z.object({
   name: z.string().min(2),
-  file: z.instanceof(File),
+  file: typeof window === 'undefined' ? z.any() : z.instanceof(FileList),
 });
 
 const PublishForm = () => {
-  const [file, setFile] = useState();
-  const [preview, setPreview] = useState();
+  const [preview, setPreview] = useState<string | undefined>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -36,58 +37,87 @@ const PublishForm = () => {
   });
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    console.log({ values });
   };
 
   const fileRef = form.register('file');
 
-  const handleSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target?.files?.[0];
+
+    if (!selectedFile) return;
+
+    const displayUrl = URL.createObjectURL(selectedFile);
+    setPreview(displayUrl);
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              {/* <FormDescription>
-                This is your public display name.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="file"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre</FormLabel>
-              <FormControl>
-                <Input
-                  type="file"
-                  multiple={false}
-                  accept="image/jpeg, image/png"
-                  {...fileRef}
-                  onChange={(e) => {
-                    handleSelectFile(e);
-                    field.onChange(e.target?.files?.[0] ?? undefined);
-                  }}
-                />
-              </FormControl>
-              {/* <FormDescription>
-                This is your public display name.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+      <form id="publish-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex gap-3 relative">
+          <div className="flex-1">
+            <div className="space-y-8 bg-secondary rounded-sm border p-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button form="publish-form" type="submit">
+                Submit
+              </Button>
+            </div>
+          </div>
+          <div className="sticky top-[120px] self-start px-3">
+            {preview ? (
+              <Image
+                src={preview}
+                alt="uploaded-image"
+                width={240}
+                height={240}
+                className="object-contain rounded-sm border h-[240px] bg-secondary"
+              />
+            ) : (
+              <div className="bg-secondary rounded-sm border h-[240px] w-[240px] flex justify-center items-center">
+                <ImageIcon size={36} />
+              </div>
+            )}
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => (
+                <FormItem className="mt-5">
+                  <FormLabel>Imagen</FormLabel>
+                  <FormControl className="mt-10">
+                    <Input
+                      type="file"
+                      multiple={false}
+                      accept="image/jpeg, image/png"
+                      {...fileRef}
+                      onChange={(e) => {
+                        handleSelectFile(e);
+                        return field.onChange(
+                          e.target?.files?.[0] ?? undefined
+                        );
+                      }}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Sube una imagen (solo se aceptan archivos .jpg y .png).
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
       </form>
     </Form>
   );
