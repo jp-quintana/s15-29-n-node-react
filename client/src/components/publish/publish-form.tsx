c87769da430ef9e4f2dced12775071be59409abe
jp-quintana <juanquintana1996@gmail.com>
2024-06-06 18:14:15 -0300

    wip5: publish form

--- client/src/components/publish/publish-form.tsx
+++ client/src/components/publish/publish-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -17,7 +17,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Image as ImageIcon } from 'lucide-react';
+import { CalendarIcon, Image as ImageIcon } from 'lucide-react';
 import Image from 'next/image';
 import { Textarea } from '../ui/textarea';
 
@@ -30,9 +30,17 @@ import {
   SelectValue,
 } from '../ui/select';
 import { CATEGORY_NAMES } from '@/lib/constants';
+import { Switch } from '../ui/switch';
+import { Label } from '../ui/label';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { format } from 'date-fns';
+import { Calendar } from '../ui/calendar';
+import { cn } from '@/lib/utils';
 
 const PublishForm = () => {
+  const [currentStep, setCurrentStep] = useState(0);
   const [preview, setPreview] = useState<string | undefined>();
+  // const [previewName, setPreviewName] = useState<string | undefined>();
 
   const form = useForm<z.infer<typeof productUploadSchema>>({
     resolver: zodResolver(productUploadSchema),
@@ -41,14 +49,19 @@ const PublishForm = () => {
       description: '',
       file: undefined,
       category: undefined,
+      is_auction: false,
+      start_date: undefined,
+      end_date: undefined,
     },
   });
+
+  const fileRef = form.register('file');
+  const { watch, trigger, getValues, resetField, clearErrors } = form;
+  const isAuction = watch('is_auction');
 
   const onSubmit = (values: z.infer<typeof productUploadSchema>) => {
     console.log({ values });
   };
-
-  const fileRef = form.register('file');
 
   const getImageData = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target?.files?.[0];
@@ -68,75 +81,267 @@ const PublishForm = () => {
     return { files, displayUrl };
   };
 
+  const handleNextStep = async () => {
+    const check = await trigger(['name', 'description', 'file', 'category'], {
+      shouldFocus: true,
+    });
+
+    if (!check) return;
+    clearErrors(['start_date', 'end_date']);
+    setCurrentStep(1);
+  };
+
+  useEffect(() => {
+    if (getValues('end_date')) resetField('end_date');
+  }, [watch('start_date')]);
+
   return (
     <Form {...form}>
       <form id="publish-form" onSubmit={form.handleSubmit(onSubmit)}>
         <div className="flex gap-3 relative">
           <div className="flex-1">
             <div className="space-y-12 bg-secondary rounded-sm border p-6">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nombre</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Nombre del producto" {...field} />
-                    </FormControl>
-                    <FormMessage className="absolute p-0 m-0" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Descripción</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        placeholder="Detalles del producto"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage className="absolute p-0 m-0" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="category"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Categoría</FormLabel>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="w-[200px] capitalize">
-                          <SelectValue placeholder="Elige una categoría" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {Object.entries(CATEGORY_NAMES).map(([key, value]) => (
-                          <SelectItem
-                            key={key}
-                            value={key}
-                            className="capitalize"
+              <>
+                {currentStep === 0 && (
+                  <>
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Nombre</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Nombre del producto"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="absolute p-0 m-0" />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="description"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Descripción</FormLabel>
+                          <FormControl>
+                            <Textarea
+                              placeholder="Detalles del producto"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage className="absolute p-0 m-0" />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="category"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Categoría</FormLabel>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
                           >
-                            {value}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                    <FormMessage className="absolute p-0 m-0" />
-                  </FormItem>
+                            <FormControl>
+                              <SelectTrigger className="w-[200px] capitalize">
+                                <SelectValue placeholder="Elige una categoría" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              {Object.entries(CATEGORY_NAMES).map(
+                                ([key, value]) => (
+                                  <SelectItem
+                                    key={key}
+                                    value={key}
+                                    className="capitalize"
+                                  >
+                                    {value}
+                                  </SelectItem>
+                                )
+                              )}
+                            </SelectContent>
+                          </Select>
+                          <FormMessage className="absolute p-0 m-0" />
+                        </FormItem>
+                      )}
+                    />
+                  </>
                 )}
-              />
-              <div className="flex items-center justify-end">
-                <Button form="publish-form" type="submit">
-                  Siguiente
+                {currentStep === 1 && (
+                  <>
+                    <FormField
+                      control={form.control}
+                      name="start_date"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                          <FormLabel>Fecha de inicio</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={'outline'}
+                                  className={cn(
+                                    'w-[240px] pl-3 text-left font-normal',
+                                    !field.value && 'text-muted-foreground'
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, 'dd/MM/yyyy')
+                                  ) : (
+                                    <span>Elegí una fecha</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) => {
+                                  const today = new Date();
+                                  today.setHours(0, 0, 0, 0);
+
+                                  const thirtyDaysFromToday = new Date(today);
+                                  thirtyDaysFromToday.setDate(
+                                    today.getDate() + 30
+                                  );
+                                  thirtyDaysFromToday.setHours(23, 59, 59, 999);
+
+                                  return (
+                                    date < today || date > thirtyDaysFromToday
+                                  );
+                                }}
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormDescription>
+                            Puedes programar la subasta con hasta un mes de
+                            anticipación.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="end_date"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                          <FormLabel>Fecha de finalización</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  disabled={!getValues('start_date')}
+                                  variant={'outline'}
+                                  className={cn(
+                                    'w-[240px] pl-3 text-left font-normal',
+                                    !field.value && 'text-muted-foreground'
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, 'dd/MM/yyyy')
+                                  ) : !getValues('start_date') ? (
+                                    <span className="truncate">
+                                      Elegí primero una fecha de inicio
+                                    </span>
+                                  ) : (
+                                    <span>Elegí una fecha</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) => {
+                                  const startDate = new Date(
+                                    getValues('start_date') as Date
+                                  );
+                                  startDate.setHours(0, 0, 0, 0);
+
+                                  const tenDaysFromStartDate = new Date(
+                                    startDate
+                                  );
+                                  tenDaysFromStartDate.setDate(
+                                    startDate.getDate() + 10
+                                  );
+                                  tenDaysFromStartDate.setHours(
+                                    23,
+                                    59,
+                                    59,
+                                    999
+                                  );
+
+                                  return (
+                                    date < startDate ||
+                                    date > tenDaysFromStartDate
+                                  );
+                                }}
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormDescription>
+                            La duración máxima de la subasta es de 10 días, con
+                            un mínimo de 1 día desde su creación.
+                          </FormDescription>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </>
+                )}
+              </>
+              <div className="flex items-center justify-between">
+                {currentStep === 0 && (
+                  <FormField
+                    control={form.control}
+                    name="is_auction"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <div className="flex items-center space-x-2">
+                            <Switch
+                              checked={field.value}
+                              onCheckedChange={field.onChange}
+                              id="is_auction"
+                            />
+                            <Label htmlFor="is_auction">Es subasta</Label>
+                          </div>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                )}
+                {currentStep === 1 && (
+                  <Button onClick={() => setCurrentStep(0)}>Atrás</Button>
+                )}
+                <Button
+                  form="publish-form"
+                  type="submit"
+                  onClick={
+                    isAuction && currentStep === 0 ? handleNextStep : undefined
+                  }
+                >
+                  {isAuction && currentStep === 0 ? 'Siguiente' : 'Crear'}
                 </Button>
               </div>
             </div>
@@ -155,33 +360,35 @@ const PublishForm = () => {
                 <ImageIcon size={36} />
               </div>
             )}
-            <FormField
-              control={form.control}
-              name="file"
-              render={({ field }) => (
-                <FormItem className="mt-5">
-                  <FormLabel>Imagen</FormLabel>
-                  <FormControl className="mt-10">
-                    <Input
-                      type="file"
-                      multiple={false}
-                      accept="image/jpeg, image/jpeg, image/png, image/webp"
-                      {...fileRef}
-                      onChange={(e) => {
-                        const { files, displayUrl } = getImageData(e);
-                        setPreview(displayUrl);
-                        return field.onChange(files);
-                      }}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    Sube una imagen (solo se aceptan archivos .jpg, .jpeg, .webp
-                    y .png).
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {currentStep === 0 && (
+              <FormField
+                control={form.control}
+                name="file"
+                render={({ field }) => (
+                  <FormItem className="mt-5">
+                    <FormLabel>Imagen</FormLabel>
+                    <FormControl className="mt-10">
+                      <Input
+                        type="file"
+                        multiple={false}
+                        accept="image/jpeg, image/jpeg, image/png, image/webp"
+                        {...fileRef}
+                        onChange={(e) => {
+                          const { files, displayUrl } = getImageData(e);
+                          setPreview(displayUrl);
+                          return field.onChange(files);
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Sube una imagen (solo se aceptan archivos .jpg, .jpeg,
+                      .webp y .png).
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
           </div>
         </div>
       </form>
--- client/src/lib/schemas/product.ts
+++ client/src/lib/schemas/product.ts
@@ -8,29 +8,49 @@ const ACCEPTED_IMAGE_TYPES = [
   'image/webp',
 ];
 
-export const productUploadSchema = z.object({
-  name: z.string().min(1, { message: '¡El nombre está vacío!' }),
-  description: z
-    .string()
-    .min(10, { message: '¡La descripción está vacía!' })
-    .max(500, {
-      message: '¡La descripción no debe superar los 500 caracteres!',
+// TODO: update schema
+export const productUploadSchema = z
+  .object({
+    name: z.string().min(1, { message: '¡El nombre está vacío!' }),
+    description: z
+      .string()
+      .min(10, { message: '¡La descripción está vacía!' })
+      .max(500, {
+        message: '¡La descripción no debe superar los 500 caracteres!',
+      }),
+    file:
+      typeof window === 'undefined'
+        ? z.any()
+        : z
+            .instanceof(FileList)
+            .refine((files) => files?.length === 1, '¡Se requiere una imagen!')
+            .refine(
+              (files) => files?.[0]?.size <= MAX_FILE_SIZE,
+              `¡El tamaño máximo del archivo es de 5MB!`
+            )
+            .refine(
+              (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+              '¡Solo se aceptan archivos .jpg, .jpeg, .png y .webp!'
+            ),
+    category: z.string({
+      message: '¡Se debe seleccionar una opción!',
     }),
-  file:
-    typeof window === 'undefined'
-      ? z.any()
-      : z
-          .instanceof(FileList)
-          .refine((files) => files?.length === 1, '¡Se requiere una imagen!')
-          .refine(
-            (files) => files?.[0]?.size <= MAX_FILE_SIZE,
-            `¡El tamaño máximo del archivo es de 5MB!`
-          )
-          .refine(
-            (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-            '¡Solo se aceptan archivos .jpg, .jpeg, .png y .webp!'
-          ),
-  category: z.string({
-    message: '¡Se debe seleccionar una opción!',
-  }),
-});
+    is_auction: z.boolean(),
+    start_date: z.date().optional(),
+    end_date: z.date().optional(),
+  })
+  .superRefine(({ is_auction, start_date, end_date }, ctx) => {
+    if (is_auction && !start_date)
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '¡Se requiere fecha de inicio!',
+        path: ['start_date'],
+      });
+
+    if (is_auction && !end_date)
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '¡Se requiere fecha de finalización!',
+        path: ['end_date'],
+      });
+  });
