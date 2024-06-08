6df3948ab85cb40c596b7232bd8d77517a98d1d9
jp-quintana <juanquintana1996@gmail.com>
2024-06-08 18:01:02 -0300

    feat: add ui logic for image upload in profile edit form

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -6,3 +6,12 @@ export const CATEGORY_NAMES: { [key: string]: string } = {
   vehicles: 'vehículos',
   'real-estate': 'propiedades',
 };
+
+export const MAX_FILE_SIZE = 5000000;
+
+export const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+];
